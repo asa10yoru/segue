93891882bf6122008aa55dba6907e0b794b97287
asa10yoru <asa10yoru@techacademy.jp>
2017-03-04 16:35:38 +0900

    合計→戻るの画面遷移まで

---
+++ segue/ResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultViewController.swift
+//  segue
+//
+//  Created by ぽんじゅうす on 2017/03/04.
+//  Copyright © 2017年 asa10yoru. All rights reserved.
+//
+
+//import Cocoa
+
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+}
